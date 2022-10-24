a096ac3bae2e014d897d894a433eb6b19d5827d7
GoDandrea <dandrea.alves@gmail.com>
2022-10-23 22:33:17 -0300

    Updates

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// Select DOM items
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+const navItems = document.querySelectorAll(".nav-item");
+
+// Set initial menu state
+let showMenu = false;
+function setMenuStartingState() {
+  if (window.innerWidth > 1170) toggleMenu();
+}
+
+// Menu button listener
+menuBtn.addEventListener("click", toggleMenu);
+
+// Menu toggle functionality
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+
+    navItems.forEach((item) => item.classList.add("show"));
+
+    // Set menu state
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+
+    navItems.forEach((item) => item.classList.remove("show"));
+
+    // Set menu state
+    showMenu = false;
+  }
+}
